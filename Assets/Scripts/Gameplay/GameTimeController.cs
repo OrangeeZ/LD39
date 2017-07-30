92c98e5db98a39415cdab563c74d6e7f62eda22e
Ivan K <ultramegacrasher@gmail.com>
2017-07-30 19:09:13 +0300

    + Very basic powers progress bar

--- Assets/Scripts/Gameplay/GameTimeController.cs
+++ Assets/Scripts/Gameplay/GameTimeController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameTimeController : MonoBehaviour
 {
+    public float TimeScaleRate => Time.timeScale;
+    
     [SerializeField]
     private GlobalGameInfo _gameInfo;
 
---
+++ Assets/Scripts/UI/Game UI/GameUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : UIScreen
+{
+    [SerializeField]
+    private Slider _powersBar;
+
+    private GameTimeController _gameTimeController;
+
+    void Update()
+    {
+        if (_gameTimeController == null)
+        {
+            _gameTimeController = FindObjectOfType<GameTimeController>();
+        }
+        
+        _powersBar.value = 1f - _gameTimeController.TimeScaleRate;
+    }
+}
