168e53ecd4355743a628db00774b9f08f5d05639
Ivan K <ultramegacrasher@gmail.com>
2017-07-30 22:30:49 +0300

    + Eat animation with effect

---
+++ Assets/Scripts/Animation/ActivateParticleSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateParticleSystem : StateMachineBehaviour
+{
+    [SerializeField]
+    private string _particleSystemName;
+    
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        
+        animator.transform.parent.gameObject.GetComponent<ParticleSystemsController>().SetParticleSystemActive(_particleSystemName, true);
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateExit(animator, stateInfo, layerIndex);
+        
+        animator.transform.parent.gameObject.GetComponent<ParticleSystemsController>().SetParticleSystemActive(_particleSystemName, false);
+    }
+}
---
+++ Assets/Scripts/Animation/ParticleSystemsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class ParticleSystemsController : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem[] _particleSystems;
+
+    void Reset()
+    {
+        _particleSystems = GetComponentsInChildren<ParticleSystem>(includeInactive: true);
+    }
+
+    void Start()
+    {
+        foreach (var each in _particleSystems)
+        {
+            each.Stop(withChildren: true);
+        }
+    }
+
+    public void SetParticleSystemActive(string name, bool isActive)
+    {
+        var particleSystem = _particleSystems.FirstOrDefault(_ => string.Compare(_.name, name, StringComparison.InvariantCultureIgnoreCase) == 0);
+
+        if (isActive)
+        {
+            particleSystem?.Play(withChildren: true);
+        }
+        else
+        {
+            particleSystem?.Stop(withChildren: true);
+        }
+    }
+}
