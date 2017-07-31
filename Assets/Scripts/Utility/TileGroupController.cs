2ac85b189eba46adf7675172e58ac5944bf9f49a
Ivan K <ultramegacrasher@gmail.com>
2017-07-31 23:22:37 +0300

    - Legacy assets + Universal tiles and environment tiles

---
+++ Assets/Scripts/Utility/TileGroupController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGroupController : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] _sprites;
+
+    [SerializeField]
+    private int _spriteIndex;
+
+    void OnValidate()
+    {
+        if (_sprites.Length == 0)
+        {
+            return;
+        }
+
+        _spriteIndex = _spriteIndex.Clamped(0, _sprites.Length);
+
+        foreach (var each in GetComponentsInChildren<SpriteRenderer>())
+        {
+            each.sprite = _sprites[_spriteIndex];
+        }
+    }
+}
